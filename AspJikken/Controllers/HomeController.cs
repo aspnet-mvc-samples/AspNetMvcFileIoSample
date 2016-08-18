0b3b5724b9df34fa798e0e8fe368fd23f60bce9c
kobake <kobake@users.sourceforge.net>
2016-08-18 17:28:07 +0900

    File Read/Write sample

--- AspJikken/Controllers/HomeController.cs
+++ AspJikken/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +11,45 @@ namespace AspJikken.Controllers
 	{
 		public ActionResult Index()
 		{
+			string dirpath = Server.MapPath("~/App_Data");
+			string filepath = Server.MapPath("~/App_Data/hoge.txt");
+
+			// Check and create directory
+			string dirinfo = "";
+			if (!Directory.Exists(dirpath))
+			{
+				dirinfo += "Directory not found: " + dirpath + "\n";
+
+				// Create
+				Directory.CreateDirectory(dirpath);
+				dirinfo += "Created directory.\n";
+			}
+			else{
+				dirinfo += "Directory already exists: " + dirpath + "\n";
+			}
+			ViewBag.DirInfo = dirinfo;
+
+			// Write file (append)
+			using (var fout = new StreamWriter(filepath, true))
+			{
+				fout.WriteLine("---");
+				fout.WriteLine("DateTime: " + DateTime.Now.ToString());
+				fout.WriteLine("Path: " + filepath);
+				fout.WriteLine("");
+			}
+
+			// Read file
+			using (var fin = new StreamReader(filepath))
+			{
+				ViewBag.FileInfo = fin.ReadToEnd();
+			}
+
+			// Remove file if reach the limit
+			if (new FileInfo(filepath).Length > 1000)
+			{
+				System.IO.File.Delete(filepath);
+			}
+	
 			return View();
 		}
 
